8a63ffa389583d00c17162a023358aa64d404ca3
Andrew <andrewchau1993@gmail.com>
2024-09-12 21:04:19 -0700

    working on model for comments

--- main_app/models.py
+++ main_app/models.py
@@ -6,6 +6,12 @@ def user_directory_path(instance, filename):
 		# file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
 		return 'user_{0}/{1}'.format(instance.user.id, filename)
 
+COMMENTS = (
+    ('R', 'Recommended'),
+    ('NR', 'Not Recommended'),
+    ('N', 'Neutral')
+)
+
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
@@ -25,3 +31,16 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse('recipe_detail', kwargs={'recipe_id': self.id})
     
+    
+class Comment(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    Comment = models.CharField(
+        max_length=2,
+        choices=COMMENTS,
+        default=COMMENTS[0][0]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.recipe.name}'
